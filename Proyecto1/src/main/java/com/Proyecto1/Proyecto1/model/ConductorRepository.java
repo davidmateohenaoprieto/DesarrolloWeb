1e5f7f92085c6702fb0e1bbbac7c65fcab9a1da6
davidmateohenaoprieto <66431287+davidmateohenaoprieto@users.noreply.github.com>
2023-03-02 11:26:47 -0500

    Delete Proyecto1 directory

--- Proyecto1/src/main/java/com/Proyecto1/Proyecto1/model/ConductorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Proyecto1.Proyecto1.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConductorRepository extends CrudRepository<Conductor,Long>{
-    
-}
